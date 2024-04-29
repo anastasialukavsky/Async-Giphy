45439bbf703be272fd216ddb86069b1d85af6337
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-29 14:25:04 -0400

    create getImage() func responsible for fetching random and not random cats images

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+async function getImage(query, random = false) {
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    let gifUrl;
+    if (random) {
+      const randomIndex = Math.floor(Math.random() * 25);
+      gifUrl = data.data[randomIndex].images.original.url;
+    } else {
+      const gifUrls = data.data.map((gif) => gif.images.original.url);
+      return gifUrls;
+    }
+
+    console.log(gifUrl);
+    return gifUrl;
+  } catch (error) {
+    console.error('Error fetching gif:', error);
+    throw error;
+  }
+}
+
+getImage('dogs', true);
+getImage('dogs', false);
+
+
+
